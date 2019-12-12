a5d33ec81288825f2b6212ee317a4de4b2213c7e
Randy Steele <steelerx155@gmail.com>
2019-12-12 20:57:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color FROM bears ORDER BY color WHERE color is between dark brown AND dark brown";
+  "SELECT color FROM bears ORDER BY color ";
 end
 
 def counts_number_of_bears_with_goofy_temperaments

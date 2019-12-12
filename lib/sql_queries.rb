18d7052db79b7a7a60f034974fb03b141a0bb4a2
Randy Steele <steelerx155@gmail.com>
2019-12-12 21:39:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,5 +30,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
- "SELECT COUNT(temperament) FROM bears WHERE temperament = 'aggressive' ";
+ "SELECT * FROM bears WHERE temperament = 'aggressive' ";
 end

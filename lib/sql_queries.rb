2203c2d0beee3f36c468ddd88e4f319d5a9f918f
Randy Steele <steelerx155@gmail.com>
2019-12-12 21:35:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament = 'goofy') FROM bears"; 
+  "SELECT COUNT(temperament) FROM bears"; 
   #FROM bears WHERE temperament = 'goofy' ";
 end
 

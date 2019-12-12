0612ecb596bff471719c03220d764f059658a243
Randy Steele <steelerx155@gmail.com>
2019-12-12 21:32:47 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,12 +21,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
- # "SELECT count FROM bears WHERE color = 'dark brown' ";
    "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown' ";
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT temperament, COUNT(temperament ='goofy') FROM bears ";
+  "SELECT temperament, COUNT(temperament) FROM bears WHERE temperament = 'goofy' ";
 end
 
 def selects_bear_that_killed_Tim
